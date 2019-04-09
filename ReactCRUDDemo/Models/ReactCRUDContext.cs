9ea1d718ff43103de96b52e15c2257894d5f1afa
Amit Viraktamath <mail2amvir@gmail.com>
2019-04-09 13:13:49 -0400

    Add files via upload

---
+++ ReactCRUDDemo/Models/ReactCRUDContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ReactCRUDDemo.Models
+{
+    public partial class ReactCRUDContext : DbContext
+    {
+        public ReactCRUDContext()
+        {
+        }
+
+        public ReactCRUDContext(DbContextOptions<ReactCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                //optionsBuilder.UseSqlServer("Server=HQ-E7490-AVIRAKTAMATH\\HQ-E7490-AVIRAK;Database=ReactCRUD;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ReactCRUD;Data Source=HQ-E7490-AVIRAK;");
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__tblCitie__F2D21A96113EA092");
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__tblEmplo__7AD04FF1122560FD");
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
